b1ab46abef261814b9ce008c1b5bc7d3ee5df8a8
lcastagno <lucho27ris@gmail.com>
2024-12-16 18:16:13 -0300

    add. pages/directives/components/routes y mucho mas

---
+++ QueComemos/src/app/pages/menu/new-menu/new-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-menu.component.html',
+  styleUrl: './new-menu.component.css'
+})
+export class NewMenuComponent {
+
+}
